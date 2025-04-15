709a41c2076e9c8cde1707b1668315df77eb2b16
mbchavez27 <epikstrykermaxchavez23@gmail.com>
2025-04-15 18:43:14 +0800

    feat: add a format date helper func

--- src/features/activityTab/ActivityTab.tsx
+++ src/features/activityTab/ActivityTab.tsx
@@ -1,18 +1,13 @@
-export function ActivityTab() {
-  const currentDate = new Date();
-  const options: Intl.DateTimeFormatOptions = {
-    year: "numeric",
-    month: "long",
-    day: "numeric",
-  };
+import { formatDate } from "@/lib/formatDate";
 
-  const formattedDate = currentDate.toLocaleDateString("en-US", options);
+export function ActivityTab() {
+  const today = formatDate(new Date());
   return (
     <>
       <div className="border-2 border-white bg-black w-1/2 rounded-md p-8 translate-y-16 h-screen">
         <div className="space-y-2">
           <h1 className="font-bold text-4xl">Activity</h1>
-          <p className="opacity-50">as of {formattedDate} </p>
+          <p className="opacity-50">as of {today} </p>
         </div>
       </div>
     </>
